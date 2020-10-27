0ffdaa42f398eec611999a690adf798748149d12
JUMAH <omegacortex@gmail.com>
2020-10-27 10:56:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/customicon/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.customicon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void b1click(View view) {
+    }
+
+    public void b2click(View view) {
+    }
+
+    public void b3click(View view) {
+    }
+}
